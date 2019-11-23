4351972f00e70e93b9086765df0b753f805a58d1
yrezar <lukas66h@gmail.com>
2019-11-23 23:12:51 +0100

    Finished Decription on Output.cpp

--- Rätsel/3-Dateiendung/Output.cpp
+++ Rätsel/3-Dateiendung/Output.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char** argv) {
-    std::cout << "Key" << std::endl;
+    int encrypted = 81273;
+    std::string plaintext = "SUPERSECRETKEY";
+    std::cout << "Key: " << "0" <<  (encrypted - 11111) << std::endl;
 }
