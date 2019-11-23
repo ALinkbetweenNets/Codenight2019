495eef5e8bcebcbf91913a2b6fcb3b2049965d7d
yrezar <lukas66h@gmail.com>
2019-11-23 23:40:25 +0100

    Possible fix for bug

--- Rätsel/3-Dateiendung/Output.cpp
+++ Rätsel/3-Dateiendung/Output.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <string>
 
 int main(int argc, char** argv) {
     int encrypted = 81273;
-    std::string plaintext = "SUPERSECRETKEY";
     std::cout << "Key: " << "0" <<  (encrypted - 11111) << std::endl;
 }
