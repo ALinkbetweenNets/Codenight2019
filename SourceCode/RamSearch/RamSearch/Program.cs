28463cc12d254f3d1b22b006d40baa76b4844576
Link2502 <carlberg.bjoern@gmail.com>
2019-11-24 03:36:15 +0100

    pass

---
+++ SourceCode/RamSearch/RamSearch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RamSearch
+{
+    class Program
+    {
+        private static string b(string s)
+        {
+            return Encoding.UTF8.GetString(Convert.FromBase64String(s));
+        }
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (string s in File.ReadAllLines("output.txt"))
+            {
+                stringBuilder.Append(s);
+            }
+            string enco = stringBuilder.ToString();
+            for (int i = 0; i < 50; i++)
+            {
+                enco = b(enco);
+            }
+            Console.ReadKey();
+        }
+    }
+}
